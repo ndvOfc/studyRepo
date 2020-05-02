19647e23b9befc464df63547c675c8cf70d4cf61
Den <ndvofc@gmail.com>
2020-05-02 21:03:27 +0500

    first commit

---
+++ project/index.js
@@ -0,0 +1,62 @@
+// //coment
+// // 
+// console.log(leftBorderWidth);
+// var leftBorderWidth = 1;
+// let second = 2;
+// const p = 3.14;
+
+// console.log(4/0);
+// console.log('string'*9);
+
+// let something;
+// console.log(something);
+
+// let person = {
+//     name: 'John',
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(person.age);
+// var arr = ['plum.png','orange.jpeg','apple.bmp'];
+
+// console.log(arr[0]);
+// console.log(typeof('gg' + 99));
+
+// console.log('https://vk.com/catalog/' + 10);
+
+// let swtcher = null;
+
+// if (swtcher) {
+//     console.log('Warning....');
+    
+// }
+
+// swtcher = 1;
+
+// if (swtcher) {
+//     console.log('Warning....');
+    
+// }
+
+// let ageCheck = confirm('Вам есть 18 лет?');
+// console.log(ageCheck);
+
+// let answer = prompt('Сколько Вам лет');
+// console.log(typeof(answer));
+
+let incr = 10,
+    dicr = 10;
+
+
+
+console.log(--incr,++dicr);
+
+
+
+
+
+
+
+
+
